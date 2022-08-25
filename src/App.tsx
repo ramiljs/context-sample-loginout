afae5115ba46f833789ad0f023b4eff961de88e2
ramildev <ramilprogrammer@gmail.com>
2022-08-25 07:20:00 +0500

    login logout context ts sample

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { UserContextProvider } from './context/userContext';
+import { User } from "./context/user";
+
+function App() {
+  return (
+    <UserContextProvider>
+      <User />
+    </UserContextProvider>
+  )
+}
+
+export default App;
---
+++ src/context/user.tsx
@@ -0,0 +1,26 @@
+import { FC, useContext } from "react";
+import { UserContext } from "./userContext";
+
+export const User: FC = () => {
+  const userContext = useContext(UserContext);
+
+  const handleLogin = () => {
+    userContext.setUser({
+      name: "Ramil",
+      email: "Ramil@gmail.com"
+    })
+  };
+
+  const handleLogout = () => {
+    userContext.setUser(null);
+  }
+
+  return (
+    <div>
+      <button onClick={handleLogin}>Login</button>
+      <button onClick={handleLogout}>Logout</button>
+      <div>User name is {userContext.user?.name}</div>
+      <div>User email is {userContext.user?.name}</div>
+    </div>
+  )
+}
