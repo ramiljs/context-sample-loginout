017473c13947169281d165e55086f58d8513e8e4
Ramil <89657343+ramiiil@users.noreply.github.com>
2022-09-29 15:44:52 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import { UserContextProvider } from './context/userContext';
+import React from "react";
+import "./App.css";
+import { UserContextProvider } from "./context/userContext";
 import { User } from "./context/user";
 
 function App() {
